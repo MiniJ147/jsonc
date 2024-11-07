729f21da3f087a69755feb6f22212bac16e92470
MiniJ147 <47308554+MiniJ147@users.noreply.github.com>
2024-11-06 22:08:34 -0500

    progress check

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <assert.h>
+
+typedef struct test test;
+struct test{
+    int val1;
+    int val2;
+};
+
+void* editTest(void* dest, size_t bytes){
+    char* cast = (char*)dest;
+    return (void*)(cast+bytes);
+}
 
 int main(){
-    printf("hello world!\n");
+    test v = {10,20};
+
+    
+    assert(&v==editTest(&v,0));
+    assert(&v.val1==editTest(&v,0));
+    assert(&v.val2==editTest(&v,4));
+    
     return 0;
 }
